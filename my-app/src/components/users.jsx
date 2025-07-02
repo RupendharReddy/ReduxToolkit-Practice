0aa0fc17c25e53e0f98872b4392e991f12b00b64
VARUGU RUPENDHAR REDDY <varugurupendharreddy@gmail.com>
2025-07-02 21:24:22 +0530

    Started AsyncThunk but not completed

---
+++ my-app/src/components/users.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUsers } from '../redux/usersSlice'
+const users = () => {
+    const users = useSelector((store)=>store.users.users)
+    useEffect(()=>{
+        const dispatch = useDispatch();
+        dispatch(fetchUsers());
+    },[])
+  return (
+    <div>
+        {
+            users.map((user)=>{
+                return <li>{user.name}</li>
+            })
+        }
+    </div>
+  )
+}
+
+export default users
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './redux/counterSlice'
 import commentSlice  from './redux/commentSlice'
 import subscriberSlice  from './redux/subscriberSlice'
+import usersSlice from './redux/usersSlice'
 export const store = configureStore({
   reducer: {
     counter:counterSlice,
     comments:commentSlice,
     Subscriber:subscriberSlice,
+    users:usersSlice
   },
 })
